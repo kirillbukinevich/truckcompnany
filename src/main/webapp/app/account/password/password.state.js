a79342eeb8c06374dad4195e9c368950dbcde109
Topolev <i.topolev.vladimir@gmail.com>
2016-10-25 07:32:07 +0300

    Get access on pages password, setting for users SUPERADMIN, ADMIN, USER

--- src/main/webapp/app/account/password/password.state.js
+++ src/main/webapp/app/account/password/password.state.js
@@ -12,7 +12,7 @@
             parent: 'account',
             url: '/password',
             data: {
-                authorities: ['ROLE_USER'],
+                authorities: ['ROLE_USER','ROLE_SUPERADMIN', 'ROLE_ADMIN'],
                 pageTitle: 'global.menu.account.password'
             },
             views: {
