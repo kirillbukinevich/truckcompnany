046ad2beefedbb2cf47288a3c2394edad7dd86d8
Topolev <i.topolev.vladimir@gmail.com>
2016-12-04 20:11:58 +0300

    Deny log on if company of user is deactivated.

--- src/main/java/com/truckcompany/web/rest/AccountResource.java
+++ src/main/java/com/truckcompany/web/rest/AccountResource.java
@@ -129,6 +129,12 @@ public class AccountResource {
         produces = MediaType.APPLICATION_JSON_VALUE)
     @Timed
     public ResponseEntity<UserDTO> getAccount() {
+
+        User userWithAuthorities = userService.getUserWithAuthorities();
+        if (userWithAuthorities != null){
+
+        }
+
         return Optional.ofNullable(userService.getUserWithAuthorities())
             .map(user -> new ResponseEntity<>(new UserDTO(user), HttpStatus.OK))
             .orElse(new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR));
