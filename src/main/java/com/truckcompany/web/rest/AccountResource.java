3b66c0f844079ddf4e7c25f9ade60d5a87b0be9f
Topolev <i.topolev.vladimir@gmail.com>
2016-12-04 23:01:49 +0300

    Fix bug which is related with uploading logo.

--- src/main/java/com/truckcompany/web/rest/AccountResource.java
+++ src/main/java/com/truckcompany/web/rest/AccountResource.java
@@ -158,7 +158,7 @@ public class AccountResource {
         return userRepository
             .findOneByLogin(SecurityUtils.getCurrentUserLogin())
             .map(u -> {
-                userService.updateUser(userDTO.getFirstName(), userDTO.getLastName(), userDTO.getEmail(),
+                userService.updateUser(userDTO.getFirstName(), userDTO.getLastName(), userDTO.getEmail(), userDTO.getLogo(),
                     userDTO.getLangKey());
                 return new ResponseEntity<String>(HttpStatus.OK);
             })
