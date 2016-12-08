884ecc43c449c4e7d0e0b6bac0a65946524ec678
Dmitry Kachurowski <dimaka4@gmail.com>
2016-12-08 21:36:00 +0300

    Add info about dilivering cost

--- src/main/webapp/app/roles/manager/routelist/manager.routelist-direction.controller.js
+++ src/main/webapp/app/roles/manager/routelist/manager.routelist-direction.controller.js
@@ -16,10 +16,10 @@
         vm.getDistance = function getDistance() {
             NgMap.getMap().then(function (map) {
                 vm.dist = 0;
-                $timeout(function() {
+                $timeout(function () {
                     angular.forEach(map.directionsRenderers[0].directions.routes[0].legs, function (value) {
                         var strDist = value.distance.value;
-                        vm.dist += strDist/1000;
+                        vm.dist += strDist / 1000;
                     });
                     vm.dist = parseInt(vm.dist);
                 }, 600);
@@ -35,14 +35,17 @@
         vm.confirmRoutelist = confirmRoutelist;
         vm.wayPoints = [];
 
-        vm.waybill = Waybill.get({id: $stateParams.id}, function () {
-            vm.checkpoints = Checkpoint.query({id: vm.waybill.routeList.id}, function () {
+        Waybill.get({id: $stateParams.id}, function (data) {
+            vm.waybill = data;
+            Checkpoint.query({id: vm.waybill.routeList.id}, function (checkpooint) {
+                vm.checkpoints = checkpooint;
                 var i = 0;
                 angular.forEach(vm.checkpoints, function (value) {
                     vm.wayPoints[i] = {location: value.name};
                     i++;
                 });
             });
+            console.log(vm.waybill);
             vm.getDistance();
         });
 
